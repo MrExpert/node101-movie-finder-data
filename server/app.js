930f8559fdddd0c788b84339e354d7248d10f47c
Sam Umirdinov <sam.umirdinov@gmail.com>
2019-04-27 11:09:02 -0700

    passes test, checks url, caches req.url

--- server/app.js
+++ server/app.js
@@ -1,8 +1,43 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const axios = require('axios');
 
 const app = express();
 
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+const cache = {};
+
+app.use(morgan('dev'));
+
+app.get('/', (req,res) => {
+    if (!!req.query.i) {
+        if (!!cache[req.query.i]) {
+            console.log('passing Cache if statement')
+            res.status(200).json(cache[req.query.i])
+        } else {
+        axios
+            .get('http://www.omdbapi.com/?i=' + req.query.i + '&apikey=8730e0e')
+            .then(response => {
+                cache[req.query.i] = response.data;
+                res.status(200).json(cache[req.query.i]);
+            }).catch (error => {
+                console.log('this is error:' + error);
+            });
+        }    
+    } else if (!!req.query.t) {
+        if (!!cache[req.query.t]) {
+            console.log('passing Cache if statement')
+            res.status(200).json(cache[req.query.t])
+        } else {
+        axios
+            .get('http://www.omdbapi.com/?t=' +  encodeURIComponent(req.query.t) + '&apikey=8730e0e')
+            .then(response => {
+                cache[req.query.t] = response.data;
+                res.status(200).json(cache[req.query.t]);
+            }).catch (error => {
+                console.log('this is error:' + error);
+            });
+        }    
+    } 
+});
 
 module.exports = app;
